e6943eed24295243440be8e14360203bb36ff77c
Akash NP <akashnp1998@gmail.com>
2025-01-22 23:01:05 +0400

    chore: simplify new goal creation
    move away from next group form idea to simple goal name and frequency

--- app/[id]/goals/new/components/new-goal-form.tsx
+++ app/[id]/goals/new/components/new-goal-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useActionState, useState } from "react";
+import { SetStateAction, useActionState, useState } from "react";
 
 import { v4 as uuid } from "uuid";
 
@@ -10,179 +10,40 @@ import { set } from "mongoose";
 type Goal = {
   objectId: string;
   goal: string;
-  level: number;
-  subgoals?: Goal[];
+  frequency: string;
 };
 
-export default function NewGoalForm() {
-  const [goal, setGoal] = useState<Goal>();
+type Props = {
+  goals: Goal[];
+  setGoals: React.Dispatch<React.SetStateAction<Goal[]>>;
+};
 
-  function handleL1Goal(goalName: string, level: number) {
-    const objectId = uuid();
-    setGoal({ objectId, goal: goalName, level });
-    console.log({ objectId, goal: goalName, level });
-  }
-
-  function handleL2Goal(parentGoalId: string, goalName: string, level: number) {
-    const objectId = uuid();
-    if (parentGoalId !== goal?.objectId) {
-      console.error("Parent goal not found");
-    }
-    if (goal) {
-      setGoal({
-        ...goal,
-        subgoals: [
-          ...(goal.subgoals || []),
-          { objectId, goal: goalName, level },
-        ],
-      });
-    }
-    console.log(goal);
-  }
-
-  function handleL3Goal(
-    parentGoalObjectId: string,
-    goalName: string,
-    level: number
-  ) {
-    const objectId = uuid();
-    let parentGoal = goal?.subgoals?.find(
-      (sg) => sg.objectId === parentGoalObjectId
-    )!;
-    const goalCopy = JSON.parse(JSON.stringify(goal));
-    goalCopy.subgoals.forEach((sg: Goal) => {
-      if (sg.objectId === parentGoalObjectId) {
-        sg.subgoals = [
-          ...(sg.subgoals || []),
-          { objectId, goal: goalName, level },
-        ];
-      }
-    });
-    setGoal(goalCopy);
-  }
-
-  function handleL4Goal(
-    parentGoalObjectId: string,
-    goalName: string,
-    level: number
-  ) {
-    const objectId = uuid();
-    let parentGoal = goal?.subgoals?.find(
-      (sg) => sg.objectId === parentGoalObjectId
-    )!;
-    const goalCopy = JSON.parse(JSON.stringify(goal));
-    goalCopy.subgoals.forEach((sg: Goal) => {
-      if (sg.objectId === parentGoalObjectId) {
-        sg.subgoals?.forEach((ssg: Goal) => {
-          if (ssg.objectId === parentGoalObjectId) {
-            ssg.subgoals = [
-              ...(ssg.subgoals || []),
-              { objectId, goal: goalName, level },
-            ];
-          }
-        });
-      }
-    });
-    setGoal(goalCopy);
+export default function NewGoalForm({ goals, setGoals }: Props) {
+  function handleAdd(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const goalName = form.goal.value;
+    const frequency = form.frequency.value;
+    console.log(goalName, frequency);
+    setGoals([...goals, { objectId: uuid(), goal: goalName, frequency }]);
+    form.reset();
   }
 
   return (
-    <form action="">
+    <form onSubmit={handleAdd}>
       <div>
-        <input type="text" name="l1Goal" id="l1Goal" placeholder="Main Goal" />
-        <button
-          type="button"
-          onClick={() => handleL1Goal("Get 3 Developer Certifications", 1)}
-        >
-          Add
-        </button>
+        <input type="text" name="goal" id="goal" placeholder="Main Goal" />
       </div>
       <br />
       <div>
-        {goal && (
-          <>
-            <h2>{goal.goal}</h2>
-            <ul>
-              {goal.subgoals?.map((sg) => (
-                <>
-                  <li key={sg.objectId}>{sg.goal}</li>
-                  <ul>
-                    {sg.subgoals?.map((ssg) => (
-                      <>
-                        <li key={ssg.objectId}>{ssg.goal}</li>
-                        <ul>
-                          {ssg.subgoals?.map((sssg) => (
-                            <>
-                              <li key={sssg.objectId}>{sssg.goal}</li>
-                              <ul>
-                                {sssg.subgoals?.map((ssssg) => (
-                                  <li key={ssssg.objectId}>{ssssg.goal}</li>
-                                ))}
-                              </ul>
-                            </>
-                          ))}
-                        </ul>
-                        <div>
-                          <input
-                            type="text"
-                            name="l3Goal"
-                            id="l3Goal"
-                            placeholder="Daily Goal"
-                          />
-                          <button
-                            type="button"
-                            onClick={() =>
-                              handleL3Goal(
-                                sg.objectId,
-                                "Get Redis Certification",
-                                3
-                              )
-                            }
-                          >
-                            Add
-                          </button>
-                        </div>
-                      </>
-                    ))}
-                  </ul>
-                  <div>
-                    <input
-                      type="text"
-                      name="l3Goal"
-                      id="l3Goal"
-                      placeholder="Weekly Goal"
-                    />
-                    <button
-                      type="button"
-                      onClick={() =>
-                        handleL3Goal(sg.objectId, "Get Redis Certification", 3)
-                      }
-                    >
-                      Add
-                    </button>
-                  </div>
-                </>
-              ))}
-              <div>
-                <input
-                  type="text"
-                  name="l2Goal"
-                  id="l2Goal"
-                  placeholder="Monthly Goal"
-                />
-                <button
-                  type="button"
-                  onClick={() =>
-                    handleL2Goal(goal.objectId, "Get Redis Certification", 2)
-                  }
-                >
-                  Add
-                </button>
-              </div>
-            </ul>
-          </>
-        )}
+        <input type="radio" name="frequency" id="daily" value="daily" />
+        <label htmlFor="daily">Daily</label>
+        <input type="radio" name="frequency" id="weekly" value="weekly" />
+        <label htmlFor="weekly">Weekly</label>
+        <input type="radio" name="frequency" id="monthly" value="monthly" />
+        <label htmlFor="monthly">Monthly</label>
       </div>
+      <button>Add</button>
     </form>
   );
 }
--- app/[id]/goals/new/page.tsx
+++ app/[id]/goals/new/page.tsx
@@ -1,15 +1,58 @@
 "use client";
 
+import { useState } from "react";
+
+import styles from "./page.module.css";
 import NewGoalForm from "./components/new-goal-form";
 
+type Goal = {
+  objectId: string;
+  goal: string;
+  frequency: string;
+};
+
 export default function AddNewGoals() {
+  const [goals, setGoals] = useState<Goal[]>([]);
+
   return (
     <main>
       <section>
         <h1>Add New Goal</h1>
       </section>
       <section>
-        <NewGoalForm />
+        <NewGoalForm goals={goals} setGoals={setGoals} />
+      </section>
+      <section className={styles.GoalList}>
+        <div>
+          {goals
+            .filter((goal) => goal.frequency == "daily")
+            .map((goal) => (
+              <div key={goal.objectId}>
+                <p>{goal.goal}</p>
+                <p>{goal.frequency}</p>
+              </div>
+            ))}
+        </div>
+        <br />
+        <div>
+          {goals
+            .filter((goal) => goal.frequency == "weekly")
+            .map((goal) => (
+              <div key={goal.objectId}>
+                <p>{goal.goal}</p>
+                <p>{goal.frequency}</p>
+              </div>
+            ))}
+        </div>
+        <br />
+        {goals
+          .filter((goal) => goal.frequency == "monthly")
+          .map((goal) => (
+            <div key={goal.objectId}>
+              <p>{goal.goal}</p>
+              <p>{goal.frequency}</p>
+            </div>
+          ))}
       </section>
     </main>
   );
